cfbc2065a2ad44956b213a457a9a2512834279b3
Martinus Yody <myodyit@gmail.com>
2021-09-23 21:44:17 +0700

    Update TugasDasPro.cpp

--- TugasDasPro.cpp
+++ TugasDasPro.cpp
@@ -5,9 +5,7 @@ using namespace std;
 int main(){
 
   string username, password;
-  cout<<"================================"<<endl;
   cout<<" Penghitung Volume Bangun Ruang"<<endl;
-  cout<<"================================"<<endl;
 
   string user = "111";
   string pw = "121";
@@ -19,9 +17,7 @@ int main(){
 
   if (username == user && password == pw){
     cout<<endl;
-    cout<<"================================"<<endl;
     cout<<"         Selamat Datang"<<endl;
-    cout<<"================================"<<endl;
 
   } else {
     cout<<endl;
@@ -48,34 +44,26 @@ int main(){
 
     double s, volume;
 
-    cout<<"================================"<<endl;
     cout<<"         VOLUME KUBUS"<<endl;
-    cout<<"================================"<<endl;
     cout<<endl;
     cout<<"        Masukan Sisi : "; cin>>s;
     volume = s*s*s;
     cout<<endl;
-    cout<<"--------------------------------"<<endl;
     cout<<"     Volume kubus Adalah "<<volume<<endl;
-    cout<<"--------------------------------"<<endl;
     break;
 
     case 2 :
 
     double p, l, t, volume2;
 
-    cout<<"================================"<<endl;
     cout<<"         VOLUME BALOK"<<endl;
-    cout<<"================================"<<endl;
     cout<<endl;
     cout<<"       Masukan Panjang : "; cin>>p;
     cout<<"       Masukan Lebar   : "; cin>>l;
     cout<<"       Masukan Tinggi  : "; cin>>t;
     volume2 = p * l * t;
     cout<<endl;
-    cout<<"--------------------------------"<<endl;
     cout<<"     Volume balok adalah "<<volume2<<endl;
-    cout<<"--------------------------------"<<endl;
     break;
 
     case 3 :
@@ -84,33 +72,27 @@ int main(){
     double Pi;
     Pi = double(22)/7;
 
-    cout<<"================================"<<endl;
     cout<<"          VOLUME TABUNG"<<endl;
-    cout<<"================================"<<endl;
     cout<<endl;
     cout<<"       Masukan Jari-Jari : "; cin>>r;
     cout<<"       Masukan Tinggi    : "; cin>>t2;
     volume3 = Pi * r * r * t2;
     cout<<endl;
-    cout<<"-------------------------------"<<endl;
     cout<<"  Volume tabung adalah "<<volume3<<endl;
-    cout<<"-------------------------------"<<endl;
     break;
 
     case 4 :
-    cout<<"          Terima Kasih!"<<endl;
     exit(0);
 
     default :
     cout<<"      Menu tidak tersedia!"<<endl;
-    cout<<"      ^^^^^^^^^^^^^^^^^^^^"<<endl;
   }
   cout<<endl;
-  cout<<"  Ingin coba yang lain? (y/n) "; cin>>ulang;
+  cout<<"  Ingin coba yang lain? (y/n)"; cin>>ulang;
   cout<<endl;
 }
   while (ulang != 'n');
-  cout<<"         Terima Kasih!"<<endl;
+  exit(0);
 
   cin.get();
   return 0;
